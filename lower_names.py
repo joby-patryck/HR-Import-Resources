713345672c247f89709fe4a8d35e2db8fe516b90
Patryck <patryck.chipman@jobyaviation.com>
2025-12-01 16:28:25 -0800

    Work in a loop now
    Now runs indefinitely until error or q

--- lower_names.py
+++ lower_names.py
@@ -3,20 +3,24 @@ import pandas
 # Get filename through stdin
 filenname = input().strip('"')
 
-data = pandas.read_csv(filenname)
+while (filenname != 'q'):
+    data = pandas.read_csv(filenname)
+    # Job Assignments file
+    if "job assignments" in filenname.lower():
+        data = data[~(data["useridnumber"].isna() | (data["useridnumber"] == ""))]
+        data["Manager email"] = data["Manager email"].fillna("#N/A")
+        for name_index in data.index:
+            data.loc[name_index, "Manager email"] = data["Manager email"][name_index].lower()
+        for name_index in data.index:
+            data.loc[name_index, "useridnumber"] = data["useridnumber"][name_index].lower()
+    # Users file
+    else:
+        data = data[~(data["idnumber"].isna() | (data["idnumber"] == ""))]
+        for name_index in data.index:
+            data.loc[name_index, "idnumber"] = data["idnumber"][name_index].lower()
 
-# Job Assignments file
-if "job assignments" in filenname.lower():
-    data = data[~(data["useridnumber"].isna() | (data["useridnumber"] == ""))]
-    data["Manager email"] = data["Manager email"].fillna("#N/A")
-    for name_index in data.index:
-        data.loc[name_index, "Manager email"] = data["Manager email"][name_index].lower()
-    for name_index in data.index:
-        data.loc[name_index, "useridnumber"] = data["useridnumber"][name_index].lower()
-# Users file
-else:
-    data = data[~(data["idnumber"].isna() | (data["idnumber"] == ""))]
-    for name_index in data.index:
-        data.loc[name_index, "idnumber"] = data["idnumber"][name_index].lower()
+    data.to_csv(filenname)
 
-data.to_csv(filenname)
+    print("Success!")
+
+    filenname = input().strip('"')
